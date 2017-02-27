fd736cbb2ac5b5091a4dfefab3ac4939051766b8
Andrew Alberding <Andrew Alberding@128.4.73.31>
2017-02-26 22:59:41 -0500

    Added

---
+++ quiz2/src/pkg/tuitioncalc.java
@@ -0,0 +1,25 @@
+package pkg;
+import java.lang.Math; 
+import java.util.Scanner;
+
+public class tuitioncalc
+{
+
+  public static void main(String[] args)
+  {
+    Scanner input = new Scanner(System.in);
+    double tuition;
+    double percent;
+    double totalcost = 0;
+    
+    System.out.println("What is the tuition cost?");
+		tuition=input.nextDouble();
+    for (int num = 0; num < 4; num++) //for all 4 years, compound the percent to the tuition cost 
+    {
+		System.out.println("What is the percent increase this year in decimal form?");
+		percent = input.nextDouble() ;
+		totalcost += tuition * (1+percent);
+	}
+    System.out.println(totalcost);
+  }
+}
